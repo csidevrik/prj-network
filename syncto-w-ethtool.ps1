cd5bd985d9b0d167c37ae61de4d99613a4ce6ea7
winadminos <carlos.sigua@gmail.com>
2025-04-05 22:53:10 -0500

    este es un nuevo proyecto

---
+++ syncto-w-ethtool.ps1
@@ -0,0 +1,27 @@
+# sync-to-w-ethtool.ps1
+
+# 1. Ruta a los repos
+$origin = "." 
+$dest   = "..\w-ethtool"
+
+# 2. Cambiar a rama w-ethtool
+Set-Location $origin
+git checkout w-ethtool
+git pull
+
+# 3. Copiar archivos (sin .git)
+robocopy $origin $dest /MIR /XD ".git" /NFL /NDL /NJH /NJS /NC /NS /NP
+
+# Confirmar en consola
+Write-Host "-OK- Archivos sincronizados con /MIR (mirror exacto)"
+
+# 4. Subir cambios
+Set-Location $dest
+git add .
+git commit -m "Sincronización desde prj-network"
+git push
+
+#5. Volver a la rama de trabajo
+.\comeback.ps1
+
+
